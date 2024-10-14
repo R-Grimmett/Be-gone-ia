9cf8dce24a730c31b8151b8f40d80c38709d165e
R-Grimmett <grimmett.rachael@gmail.com>
2024-10-14 18:22:30 +1000

    work on problem view, css updates

--- controllers/problemViewController.js
+++ controllers/problemViewController.js
@@ -26,6 +26,62 @@ function ready() {
 function loadProblemData(problemData) {
     console.log(problemData);
     problemHero.style.backgroundImage = problemData.imgSrc !== "" ? `url(${problemData.imgSrc})` : "url('../img/background/gradient-green.jpg')";
+    populateName(problemData.common, problemData.scientific);
+    populateSymptoms(problemData.leafTags, problemData.flowerTags, problemData.stemTags, problemData.rootTags, problemData.growthTags, problemData.wholeTags);
+    populateInfo(problemData.treatment, problemData.information);
 }
 
 function loadNoProblem(statusCode) {}
+
+function populateName(commonName, scientificName) {
+    const heroCommon = document.createElement('h1');
+    heroCommon.innerHTML = `${commonName[0]}`;
+    problemHero.appendChild(heroCommon);
+
+    const heroScientific = document.createElement('p');
+    if(scientificName !== "" && scientificName !== null && scientificName !== undefined) {
+        heroScientific.innerHTML = `${scientificName}`;
+        problemHero.appendChild(heroScientific);
+    }
+
+    const commonDiv = document.createElement("div");
+    const commonList = document.createElement('ul');
+    if(commonName.length > 1) {
+        commonDiv.innerHTML = `<h4>Also called:</h4>`;
+        commonDiv.id = 'allCommon';
+        for (let i = 1; i < commonName.length; i++) {
+            const entry = document.createElement("li");
+            entry.innerHTML = commonName[i];
+            commonList.appendChild(entry);
+        }
+        commonDiv.appendChild(commonList);
+        mainContent.appendChild(commonDiv);
+    }
+}
+
+//TODO add in the descriptions
+function populateSymptoms(leafTags, flowerTags, stemTags, rootTags, growthTags, wholeTags) {
+    console.log(leafTags);
+    console.log(flowerTags);
+    console.log(stemTags);
+    console.log(rootTags);
+    console.log(growthTags);
+    console.log(wholeTags);
+}
+
+function populateInfo(treatmentData, infoData) {
+    if(treatmentData !== "" && treatmentData !== null && treatmentData !== undefined) {
+        const treatDiv = document.createElement('div');
+        treatDiv.id = 'problem-treatment';
+        treatDiv.innerHTML = `<h3>Treatment Options:</h3>${treatmentData}`;
+        mainContent.appendChild(treatDiv);
+    }
+
+
+    if(infoData !== "" && infoData !== null && infoData !== undefined) {
+        const infoDiv = document.createElement('div');
+        infoDiv.id = `view-info`;
+        infoDiv.innerHTML = `<h3>Information:</h3>${infoData}`;
+        mainContent.appendChild(infoDiv);
+    }
+}
